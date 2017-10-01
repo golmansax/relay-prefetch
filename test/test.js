eeb6de125b0209714f279e846717005fa68133ca
Holman Gao <holman@golmansax.com>
2017-10-01 17:29:06 -0400

    Get a test running

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+require('isomorphic-fetch');
+const fetchMock = require('fetch-mock');
 const { expect } = require('chai');
 const Relay = require('react-relay');
 const relayPrefetch = require('../');
@@ -5,14 +7,33 @@ const relayPrefetch = require('../');
 const MyComponent = () => null;
 const MyContainer = Relay.createContainer(MyComponent, {
   fragments: {
+    starship: () => Relay.QL`fragment on Starship { id }`,
   },
 });
 
 describe('relay-prefetch', function () {
+  let mock;
+
+  beforeEach(function () {
+    mock = fetchMock.mock('/graphql', 200);
+  });
+
+  afterEach(function () {
+    mock.restore();
+  });
+
   it('does not throw errors', function () {
     relayPrefetch({
       Component: MyContainer,
-      route: {},
+      route: {
+        name: 'TestRoute',
+        queries: {
+          starship: () => Relay.QL`query { starship(id: $id) }`,
+        },
+        params: { id: 'testId' },
+      },
     });
+
+    // expect(fetchMock.called('/graphql')).to.equal(true);
   });
 });
